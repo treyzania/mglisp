0efb283c7d57b4bd768a7058fb17b61593bb787c
treyzania <treyzania@gmail.com>
2018-01-04 03:13:07 -0500

    Added lambda and define intrinsics.

--- vm/intrinsics.rs
+++ vm/intrinsics.rs
@@ -27,3 +27,57 @@ impl PartialEq for MgIntrinsic {
 }
 
 impl Eq for MgIntrinsic {}
+
+mod core {
+
+    use std::rc::*;
+
+    use sexp::Sexp;
+
+    use Atom;
+    use LispFunction;
+    use Env;
+    use EvalError;
+    use eval;
+
+    pub fn mgi_lambda(args: Vec<Sexp>, env: &mut Env) -> Result<Atom, EvalError> {
+
+        if args.len() != 2 {
+            return Err(EvalError::Msg("invalid form for lambda".into()));
+        }
+
+        let mut names = Vec::new();
+        match args[0].clone() {
+            Sexp::List(l) => for a in l {
+                match a {
+                    Sexp::Symbol(s) => names.push(s),
+                    _ => return Err(EvalError::Msg("invalid form for lambda".into()))
+                }
+            },
+            _ => return Err(EvalError::Msg("invalid form for lambda".into()))
+        };
+
+        Ok(Atom::Func(LispFunction::Lambda(Rc::new(args[1].clone()), env.clone(), names)))
+
+    }
+
+    pub fn mgi_define(args: Vec<Sexp>, env: &mut Env) -> Result<Atom, EvalError> {
+
+        if args.len() != 2 {
+            return Err(EvalError::Msg("invalid form for define".into()));
+        }
+
+        let binding = match args[0].clone() {
+            Sexp::Symbol(s) => s,
+            _ => return Err(EvalError::Msg("invalid form for define".into()))
+        };
+        let value = eval(args[1].clone(), &mut env.clone())?;
+
+        // This is where we actually mutate the environment.
+        env.0.insert(binding, Rc::new(value)); // FIXME Make this not shit.
+
+        Ok(Atom::Null)
+
+    }
+
+}
